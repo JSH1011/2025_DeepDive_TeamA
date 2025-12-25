153d0eb1161f970574e4c6b2a220f2c80e0b6b42
chikumo <yangju058@gmail.com>
2025-12-25 21:43:36 +0900

    12345

--- Assets/Scripts/SceneA/BlackCircleMover.cs
+++ Assets/Scripts/SceneA/BlackCircleMover.cs
@@ -6,6 +6,8 @@ public class BlackCircleMover : MonoBehaviour
     // aaaaaaaaaaaaaaaaaaa
     // aaaaaaaaaaa
 
+    // 이것은 실험용2
+
     [Header("원 슬롯들 (드래그로 넣거나, Auto Collect 사용)")]
     [SerializeField] private CircleSlot[] slots;
 
